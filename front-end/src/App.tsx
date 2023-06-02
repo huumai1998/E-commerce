8fb6e619f2c6b3320fbf4c852e3c92c8ce6b1bc0
huumai1998 <huumai0918@gmail.com>
2023-06-02 07:00:54 -0600

    save

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,10 +1,22 @@
 import { useState } from 'react'
+import { sampleProducts } from './data'
 
 function App() {
 
   return (
     <>
     <header>Hallo</header>
+    <div>
+      
+      <ul>
+      { sampleProducts.map(product => 
+        <li>
+          <img src={product.image} width={50} height={50}/>
+          <h2>{product.name}</h2>
+        </li>)
+      }
+      </ul>
+    </div>
     </>
   )
 }
---
+++ front-end/src/data.ts
@@ -0,0 +1,40 @@
+import { Product } from "./types/Product";
+
+export const sampleProducts:Product[] = [
+    {
+        name: 'Nike Slim Shirt',
+        slung: 'asdf',
+        category: 'scs',
+        image: '../images/1.jpg',
+        price: 23,
+        countInStock: 1000,
+        brand: 'Nike',
+        rating: 1,
+        numReviews: 10,
+        description: 'fadfaf',
+    },
+    {
+        name: 'Nike Slim Shirt',
+        slung: 'asdf',
+        category: 'scs',
+        image: '../images/2.jpg',
+        price: 23,
+        countInStock: 1000,
+        brand: 'Nike',
+        rating: 1,
+        numReviews: 10,
+        description: 'fadfaf',
+    },
+    {
+        name: 'Nike Slim Shirt',
+        slung: 'asdf',
+        category: 'scs',
+        image: '../images/3.jpg',
+        price: 23,
+        countInStock: 1000,
+        brand: 'Nike',
+        rating: 1,
+        numReviews: 10,
+        description: 'fadfaf',
+    }
+]
