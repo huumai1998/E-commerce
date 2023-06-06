22a7310e51d5bd707d3f472f13d1b1c4b9318266
huumai1998 <huumai0918@gmail.com>
2023-06-06 09:07:13 -0600

    style setup

--- front-end/src/components/nav.tsx
+++ front-end/src/components/nav.tsx
@@ -5,8 +5,17 @@ export const Nav = () => {
   return (
     <>
     <section className="nav">
-      <div className=''>
-      <Link to="/destination">Go to Destination</Link>
+      <div className='nav-container'>
+        <div className="logo-links">
+          {/* <img src="">Logo here</img> */}<p>Logo</p>
+          <Link to="/">Link Name here</Link>
+          <Link to="/">Link Name here</Link>
+          <Link to="/">Link Name here</Link>
+        </div>
+        <div className="login-register-pages">
+          <Link to="">Login</Link>
+          <Link to="">Register</Link>
+        </div>
       </div>
     </section>
     </>
