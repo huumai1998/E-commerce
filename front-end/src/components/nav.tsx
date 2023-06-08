b6d300bbb5a76312b0c6e008f9f70441dc4121a5
huumai1998 <huumai0918@gmail.com>
2023-06-08 07:12:23 -0600

    wtf

--- front-end/src/components/nav.tsx
+++ front-end/src/components/nav.tsx
@@ -1,32 +1,27 @@
-
+import React from 'react';
 import { Link } from "react-router-dom";
 
-export const Nav = () => {
+export const Nav: React.FC = () => {
   return (
     <>
     <div className="nav">
-      <div className='nav-container'>
-        <div className="logo-links">
-          <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a className="navbar-brand" href="#">Navbar</a>
-            {/* burgur bar */}
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-              <div className="navbar-nav">
-                <Link className="nav-item nav-link active" to="#">Home <span className="sr-only">(current)</span></Link>
-                <Link className="nav-item nav-link" to="#">Features</Link>
-                <Link className="nav-item nav-link" to="#">Pricing</Link>
-              </div>
+        <nav className="navbar">
+          <div className="navbar-container">
+          <div className="navbar-left">
+            <div className='logo'>
+              <Link to="/" className="navbar-logo"><img /><p>Logo</p></Link>
             </div>
-          </nav>
+            <div className='nav-link'>
+              <Link to="/" className="navbar-link">Home</Link>
+              <Link to="/" className="navbar-link">Feature</Link>
+            </div>
+          </div>
+          <div className="navbar-right">
+            <p><a href="/login" className="navbar-link">Login</a> <span>/</span>
+            <a href="/register" className="navbar-link">Register</a></p>
+          </div>
         </div>
-        <div className="login-register-pages">
-          {/* <Link to="">Login</Link>
-          <Link to="">Register</Link> */}
-        </div>
-      </div>
+      </nav>
     </div>
     </>
   )
