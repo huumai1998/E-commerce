749cbf191b6e1f0869c42715ff4ddcb80a93a38b
huumai1998 <huumai0918@gmail.com>
2023-06-10 10:02:53 -0600

    addModel

---
+++ back-end/back-end/Models/userModel.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace back_end.Models
+{
+    public class userModel
+    {
+        
+    }
+}
