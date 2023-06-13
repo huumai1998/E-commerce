47210102a1c0cf90b1661182cb8359a69dd0ecf4
huumai1998 <huumai0918@gmail.com>
2023-06-13 15:21:22 -0600

    signinAPI

--- back-end-backup/src/controllers/auth.controllers.ts
+++ back-end-backup/src/controllers/auth.controllers.ts
@@ -5,18 +5,33 @@ import bcrypt from 'bcryptjs';
 import { Request, Response } from 'express'
 
 export const login = asyncHandler(async (req: Request, res: Response) => {
-    const user = await UserModel.findOne({ email: req.body.email });
-    if (user) {
-        if(bcrypt.compareSync(req.body.password, user.password)) {
-            res.send({
-                _id: user._id, 
-                name: user.name,
-                email: user.email,
-                isAdmin: user.isAdmin,
-                token: generateToken(user),
-            })
-            return
+    try {
+        const user = await UserModel.findOne({ email: req.body.email });
+        if (user) {
+            if(bcrypt.compareSync(req.body.password, user.password)) {
+                res.send({
+                    _id: user._id, 
+                    name: user.name,
+                    email: user.email,
+                    isAdmin: user.isAdmin,
+                    token: generateToken(user),
+                })
+                return
+            } 
+        }
+        if (!user) 
+        {
+            res.status(401).json({ message: 'Invalid email!' })
         } 
+        else if (!bcrypt.compareSync(req.body.password, user.password)) 
+        { 
+            res.status(401).json({ message: 'Invalid password!' })
+        } 
+        else 
+        {
+            res.status(401).json({ message: 'Invalid email or password' })
+        }
+    } catch (error) {
+        res.status(500).json('Sever error!')
     }
-    res.status(401).json({ message: 'Invalid email or password' })
 })
