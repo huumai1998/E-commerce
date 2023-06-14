0fcee8ff378774ceeed4a2986d136c7200c12086
huumai1998 <huumai0918@gmail.com>
2023-06-13 22:56:22 -0600

    updateBackend-prodcut

--- back-end-backup/src/controllers/product.controllers.ts
+++ back-end-backup/src/controllers/product.controllers.ts
@@ -6,3 +6,14 @@ export const product = asyncHandler(async (req, res) => {
     const products = await ProductModel.find()
     res.json(products)
 })
+
+
+// GET: api/slug/:slug
+export const getSlugProduct = asyncHandler(async (req, res) => {
+    const product = await ProductModel.findOne({ slug: req.params.slug })
+    if (product) {
+      res.json(product)
+    } else {
+      res.status(404).json({message: 'Product Not Found'})
+    }
+})
--- back-end-backup/src/routers/product.routes.ts
+++ back-end-backup/src/routers/product.routes.ts
@@ -1,7 +1,5 @@
 import express from "express"
-import asyncHandler from 'express-async-handler';
-import { ProductModel } from "../models/product";
-import { product } from "../controllers/product.controllers";
+import { getSlugProduct, product } from "../controllers/product.controllers";
 
 
 export const productRouter = express.Router()
@@ -9,14 +7,4 @@ export const productRouter = express.Router()
 productRouter.get('/', product)
 
 // GET: api/name/tshirt
-productRouter.get(
-  '/:slug',
-  asyncHandler(async (req, res) => {
-    const product = await ProductModel.findOne({ slug: req.params.slug })
-    if (product) {
-      res.json(product)
-    } else {
-      res.status(404).json({message: 'Product Not Found'})
-    }
-  })
-)
+productRouter.get('/:slug', getSlugProduct)
